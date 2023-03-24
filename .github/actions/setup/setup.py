5574124e48f107e9876c54b82c0f5a702b00362a
xiplus <huangxuanyuxiplus@gmail.com>
2023-03-24 19:39:02 +0800

    Write config

--- .github/actions/setup/setup.py
+++ .github/actions/setup/setup.py
@@ -2,8 +2,39 @@ import os
 
 os.chdir(os.environ.get('GITHUB_WORKSPACE'))
 
-print('username', os.environ.get('USERNAME'))
-print('password', os.environ.get('PASSWORD'))
-print('family', os.environ.get('FAMILY'))
-print('mylang', os.environ.get('MYLANG'))
-print('url', os.environ.get('URL'))
+username = os.environ.get('USERNAME', '')
+botname = os.environ.get('BOTNAME', '')
+password = os.environ.get('PASSWORD', '')
+family = os.environ.get('FAMILY')
+mylang = os.environ.get('MYLANG')
+url = os.environ.get('URL')
+
+if url:
+    family = 'mysite'
+    MYLANG = 'mysite'
+
+print('username', username)
+print('family', family)
+print('mylang', MYLANG)
+print('url', url)
+
+with open('user-config.py', 'w', encoding='utf8') as f:
+    if family:
+        f.write("family = '{}'\n".format(family))
+    if mylang:
+        f.write("mylang = '{}'\n".format(mylang))
+    if family and mylang:
+        f.write("usernames['{}']['{}'] = u'{}'\n".format(
+            family,
+            mylang,
+            username,
+        ))
+    if url:
+        f.write("family_files['mysite'] = '{}'\n".format(url))
+
+with open('user-password.py', 'w', encoding='utf8') as f:
+    f.write("(u'{}', BotPassword(u'{}', u'{}'))\n".format(
+        username,
+        botname,
+        password,
+    ))
